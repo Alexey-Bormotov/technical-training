77d004d76c8e6b8c423f3233c14a85d95808567d
Alexey-Bormotov <di-devil@yandex.ru>
2023-05-18 18:16:22 +0700

    Created basic model, views and security settings

---
+++ estate/models/models.py
@@ -0,0 +1,51 @@
+from dateutil.relativedelta import relativedelta
+
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Real Estate Property'
+
+    def _default_date_availability(self):
+        return fields.Date.context_today(self) + relativedelta(months=3)
+
+    name = fields.Char('Title', required=True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date(
+        'Available From',
+        copy=False,
+        default=lambda self: self._default_date_availability()
+    )
+    expected_price = fields.Float('Expected price', required=True)
+    selling_price = fields.Float('Selling price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living area (sq.m.)')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden area (sq. m.)')
+    garden_orientation = fields.Selection(
+        string='Garden orientation',
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ]
+    )
+
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        string='Status',
+        required=True,
+        copy=False,
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ],
+        default='new'
+    )
