c132afdd8f26846d1d351f21dfa41cca2bf33025
Алексей Бормотов <di-devil@yandex.ru>
2023-07-01 10:11:53 +0700

    Modified res.users model and views with inheritance

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -1,10 +1,11 @@
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
+    _description = 'Users, upgraded to salesmen'
 
-    property_ids = One2many(
+    property_ids = fields.One2many(
         'estate.property',
         'salesman_id',
         string='Properties',
